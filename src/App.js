4ee18e059210b6b770674c8db46b22289839e055
Gaston <gaston.cba97@gmail.com>
2021-06-11 12:59:29 -0300

    Primera Subida

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bienividos a Dessert Now!</h1>
+      <h3>Servicio de Delivery de Postres!</h3>
+      <p>Mira nuestro catalogo , pedilo y esta en un casa ahora!</p>
     </div>
   );
 }
