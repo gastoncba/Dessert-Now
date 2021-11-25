545c950cce0261e1dadfb225d37315824356f88c
Gaston <gaston.cba97@gmail.com>
2021-11-25 11:24:31 -0300

    se cambian margenes

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Item from '../Item/Item';
 import {Card} from 'semantic-ui-react';
-
+import './ItemList.css'
 export default function ItemList({items}) {
     
     return (
-            <Card.Group>
+            // <Card.Group>
+            <div className={'card-group'}>
                 {items.map(item => {
                     return(
                     <Item item={item} key={item.id}></Item>
                     )
                 })}
-            </Card.Group>
+            </div>
+            // </Card.Group>
     )
 
 }
