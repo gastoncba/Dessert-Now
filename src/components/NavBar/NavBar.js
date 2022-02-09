e79a77fd50b47ad6469056110d354618fcb32b56
Gaston Barrionuevo <gaston.cba97@gmail.com>
2022-02-09 15:26:47 -0300

    se cambia metodos para pedido

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext, useEffect} from 'react'
+import React, {useContext} from 'react'
 import CartWidget from '../CartWidget/CartWidget'
 import { Link } from 'react-router-dom';
 import './NavBar.css';
